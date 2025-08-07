be2f41786556fc93d731a788215dda28b4575369
ShrenaBajiko <41513194+ShrenaBajiko@users.noreply.github.com>
2025-08-07 13:34:13 +1000

    Add StudentProcessor interface for student data operations
    Created StudentProcessor interface to define contract for processing student data.
    This includes methods to read students from a file and to filter and display students
    whose total marks fall below a user-defined threshold.

---
+++ Assessment2Fundamental/StudentInterface.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of interface StudentInterface here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public interface StudentInterface
+{
+    void readStudentsFromFile(String filename);
+    void StudentsDetailBelowThreshold(int threshold);
+}
